03ef13920dfe0b3e3358afa40e43f8f723c6cf9f
Alejandro gg <alejandro.gongar.2@educa.jcyl.es>
2024-11-12 11:10:54 +0000

    nuvo

--- java.js
+++ java.js
@@ -18,19 +18,20 @@ let result11 =ingredientes[ingredientes.length -1].length;
 let result12 = alumno.toUpperCase().indexOf("GARCÍA")>0;
 let result13 = alumno.slice(0,alumno.indexOf(" "));
 
-let palabra = "0";
-do {
-  let palabra = "0";
+let palabra = " ";
+ do {
   palabra = prompt("CONTRASEÑA:");
-  if(palabra.toLowerCase() === "patata")
+  for(let i = 1; i <= 3; i++){if(palabra.toLowerCase() === "patata")
     {
       alert("CORRECTO");
       break;
     }
-  else 
+    else 
     {
     alert("ERROR");
     }
+  }
+  
 }
 while(palabra.toLowerCase()!== "patata");
 
